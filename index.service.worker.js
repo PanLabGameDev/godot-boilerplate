88165912e58a1b8fb7c2b38a5eb954b462ecfcce
h4de5 <h4de5@users.noreply.github.com>
2024-07-24 17:58:13 +0000

    Deploying to gh-pages from  @ d97f495ce0895ab6ef1776038749141ae3208318 🚀

--- index.service.worker.js
+++ index.service.worker.js
@@ -3,12 +3,12 @@
 // that they need an Internet connection to run the project if desired.
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
-const CACHE_VERSION = "1660168800|2066793";
+const CACHE_VERSION = "1721843859|2799073";
 const CACHE_PREFIX = "Game-sw-cache-";
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
 const OFFLINE_URL = "index.offline.html";
 // Files that will be cached on load.
-const CACHED_FILES = ["index.html","index.js","index.offline.html","index.icon.png","index.apple-touch-icon.png"];
+const CACHED_FILES = ["index.html","index.js","index.offline.html","index.icon.png","index.apple-touch-icon.png","index.worker.js","index.audio.worklet.js"];
 // Files that we might not want the user to preload, and will only be cached on first load.
 const CACHABLE_FILES = ["index.wasm","index.pck"];
 const FULL_CACHE = CACHED_FILES.concat(CACHABLE_FILES);
