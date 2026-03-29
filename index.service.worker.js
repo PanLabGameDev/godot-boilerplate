9ca279b2a104a7f6eb98d8a3d958716c2020552f
h4de5 <h4de5@users.noreply.github.com>
2026-03-29 22:34:37 +0000

    Deploying to gh-pages from  @ b24d4f912aed8737013a719ca233cae36c104ecd 🚀

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1770482280|2544170';
+const CACHE_VERSION = '1774823623|2781291';
 /** @type {string} */
 const CACHE_PREFIX = 'Game-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
